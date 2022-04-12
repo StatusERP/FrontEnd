8033c0e2cfdf5289ccea76a31e43a4f8fae29a04
Carlos roberto velasquez <carlosrobertovelasquez@gmail.com>
2022-04-11 18:18:18 -0600

    Se addiciona el register

--- src/app/page/user/register/register.component.ts
+++ src/app/page/user/register/register.component.ts
@@ -1,3 +1,6 @@
+import { UserApiService } from './../services/api/user-api.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
 	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-	Roles: any = ['Admin', 'Author', 'Reader'];
+	constructor(private _formBuilder: FormBuilder, private _router: Router, private _userApiService: UserApiService) {
+		this._loadFormGroup();
+	}
+	formGroup!: FormGroup;
+	private _loadFormGroup(): void {
+		this.formGroup = this._formBuilder.group({
+			firtstName: [null, Validators.required],
+			lastName: [null, Validators.required],
+			typeDocument: [null, Validators.required],
+			documentNumber: [null, Validators.required],
+			email: [null, Validators.required],
+			password: [null, Validators.required],
+			confirmPassword: [null, Validators.required],
+			age: null
+		});
+	}
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { ChanelHeaderService } from './../../../services/local/chanel-header.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-sidebar',
 	templateUrl: './sidebar.component.html',
 	styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {}
+export class SidebarComponent implements OnInit {
+	constructor(private _chanelHeaderService: ChanelHeaderService) {}
+	showUser = false;
+	ngOnInit(): void {
+		this._chanelHeaderService.channelHeader$.subscribe((value) => {
+			console.log(value);
+			console.log('estamos en header');
+		});
+	}
+}

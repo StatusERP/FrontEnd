2ece198e533d0f3e389ba022428c7b64e04287ac
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2022-04-12 19:09:49 -0600

    se aceptan los cambios

--- src/app/page/user/register/register.component.ts
+++ src/app/page/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 	Roles: any = ['Admin', 'Author', 'Reader'];
+	/**
+	 *
+	 */
+	constructor(private _formBuilder: FormBuilder) {
+		this._loadFormGroup();
+	}
+	formGroup!: FormGroup;
+	private _loadFormGroup(): void {
+		this.formGroup = this._formBuilder.group({
+			firstName: [null, Validators.required],
+			lastName: [null, Validators.required],
+			documentoNumer: [null, Validators.required],
+			email: [null, [Validators.required]],
+			password: [null, [Validators.required, Validators.email]],
+			confirmPassword: [null, Validators.required],
+			age: null
+		});
+	}
 }
